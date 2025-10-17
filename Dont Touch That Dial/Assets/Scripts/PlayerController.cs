504f291e97c64a994246d87c3f3ae56b9055e79a
Jacob Lee <jlee57@iu.edu>
2025-10-16 22:50:17 -0400

    Implemented Barriers to the Board
    The player can no longer bypass the sides of the board.

--- Dont Touch That Dial/Assets/Scripts/PlayerController.cs
+++ Dont Touch That Dial/Assets/Scripts/PlayerController.cs
@@ -4,11 +4,15 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     public GameObject player;
-    private Vector2 upperBarrier = Vector2.up;
-    private Vector2 lowerBarrier = Vector2.down;
-    private Vector2 leftBarrier = Vector2.left;
-    private Vector2 rightBarrier = Vector2.right;
     private Vector2 currPlayerPosition = new Vector2(0,-2);
+    private float upperBarrier = 0;
+    private float lowerBarrier = -4;
+    private float leftBarrier = -2;
+    private float rightBarrier = 2;
+    private bool disableUp;
+    private bool disableDown;
+    private bool disableLeft;
+    private bool disableRight;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -19,23 +23,36 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        BarrierCheck();
         
-        if (Input.GetKeyDown(KeyCode.LeftArrow) || Input.GetKeyDown(KeyCode.A)){
+        if ((Input.GetKeyDown(KeyCode.LeftArrow) || Input.GetKeyDown(KeyCode.A)) && !disableLeft){
             currPlayerPosition.x -= 1;
-            Debug.Log("left");
         }
-        if (Input.GetKeyDown(KeyCode.RightArrow) || Input.GetKeyDown(KeyCode.D))
+        if ((Input.GetKeyDown(KeyCode.RightArrow) || Input.GetKeyDown(KeyCode.D)) && !disableRight)
         {
             currPlayerPosition.x += 1;
         }
-        if (Input.GetKeyDown(KeyCode.UpArrow) || Input.GetKeyDown(KeyCode.W))
+        if ((Input.GetKeyDown(KeyCode.UpArrow) || Input.GetKeyDown(KeyCode.W)) && !disableUp)
         {
             currPlayerPosition.y += 1;
         }
-        if (Input.GetKeyDown(KeyCode.DownArrow) || Input.GetKeyDown(KeyCode.S))
+        if ((Input.GetKeyDown(KeyCode.DownArrow) || Input.GetKeyDown(KeyCode.S)) && !disableDown)
         {
             currPlayerPosition.y -= 1;
         }
         player.gameObject.transform.position = currPlayerPosition;
+    }
+
+
+    public void BarrierCheck()
+    {
+        if (player.gameObject.transform.position.y == upperBarrier){ disableUp = true; }
+        else { disableUp = false; }
+        if (player.gameObject.transform.position.y == lowerBarrier) { disableDown = true; }
+        else { disableDown = false; }
+        if (player.gameObject.transform.position.x == leftBarrier) { disableLeft = true; }
+        else { disableLeft = false; }
+        if (player.gameObject.transform.position.x == rightBarrier) { disableRight = true; }
+        else { disableRight = false; }
     }
 }
