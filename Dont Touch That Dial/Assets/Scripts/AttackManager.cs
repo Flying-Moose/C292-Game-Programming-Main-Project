da0347a354145f0aefd25a11172495d80fa6238a
Jacob Lee <jlee57@iu.edu>
2025-11-04 17:12:34 -0500

    Finished Country's Attacks (Mostly)
    Still messing around with randomness a little but attacks are all there and loop

--- Dont Touch That Dial/Assets/Scripts/AttackManager.cs
+++ Dont Touch That Dial/Assets/Scripts/AttackManager.cs
@@ -1,3 +1,4 @@
+using System.Security.Cryptography;
 using Unity.VisualScripting;
 using UnityEngine;
 
@@ -24,9 +25,8 @@ public class AttackManager : MonoBehaviour
     public GameObject CLpaper2;
 
     private float attackPause;
-    private bool temp;
-    private bool temp2;
-    private bool instantiated;
+    private float randInt;
+    private bool ifRand = true;
 
     GameObject bullet1 = null;
     GameObject bullet2 = null;
@@ -67,45 +67,69 @@ public class AttackManager : MonoBehaviour
         {
             if (attackPause > 26.4)
             {
+                OneRandom(0, -4);
+                bomb.transform.position = new Vector3(-15, randInt);
                 bullet1.transform.position += Vector3.down * Time.deltaTime * 7;
                 bullet2.transform.position += Vector3.down * Time.deltaTime * 7;
                 bullet3.transform.position += Vector3.down * Time.deltaTime * 7;
-                attackPause -= Time.deltaTime;
             }
             else if (attackPause > 25.9)
             {
+                ifRand = true;
                 bullet2.gameObject.transform.position = new Vector3(0, 10);
                 bullet1.transform.position += Vector3.down * Time.deltaTime * 7;
                 bullet3.transform.position += Vector3.down * Time.deltaTime * 7;
                 swipe.gameObject.transform.position = new Vector3(0, -2);
                 splitBullet1.gameObject.transform.position = new Vector3(0, -3);
                 splitBullet2.gameObject.transform.position = new Vector3(0, -3);
-                attackPause -= Time.deltaTime;
             }
-            else if (attackPause > 22)
+            else if (attackPause > 24)
             {
+                OneRandom(0, -4);
+                bomb2.transform.position = new Vector3(15, randInt);
                 bullet1.transform.position += Vector3.down * Time.deltaTime * 7;
                 bullet3.transform.position += Vector3.down * Time.deltaTime * 7;
-                splitBullet1.transform.position -= new Vector3(-1,-1) * Time.deltaTime * 7;
-                splitBullet2.transform.position -= new Vector3(1, 1) * Time.deltaTime * 7;
+                splitBullet1.transform.position -= new Vector3(-0.5f, -1) * Time.deltaTime * 7;
+                splitBullet2.transform.position -= new Vector3(0.5f, 1) * Time.deltaTime * 7;
                 swipe.gameObject.transform.position = new Vector3(0, 10);
-                attackPause -= Time.deltaTime;
             }
-            else if (attackPause > 18)
+            else if (attackPause > 20)
             {
+                ifRand = true;
+                splitBullet1.transform.position -= new Vector3(-0.5f, -1) * Time.deltaTime * 7;
+                splitBullet2.transform.position -= new Vector3(0.5f, 1) * Time.deltaTime * 7;
                 bomb.transform.position += Vector3.right * Time.deltaTime * 8;
+            }
+            else if (attackPause > 19.5)
+            {
                 bullet1.gameObject.transform.position = new Vector3(-2, 11);
                 bullet3.gameObject.transform.position = new Vector3(2, 11);
-                attackPause -= Time.deltaTime;
-            }
-            else if (attackPause > 17.5)
-            {
                 blast.transform.position = bomb.transform.position;
                 bomb.SetActive(false);   
-                attackPause -= Time.deltaTime;
-            } else if (attackPause > 16)
+            } else if (attackPause > 15.5)
             {
-                blast.transform.position = new Vector3(-15, 2);
+                bomb.SetActive(true);
+                bomb.transform.position = new Vector3(-15, -2);
+                blast.transform.position = new Vector3(-15, -2);
+                bomb2.transform.position += Vector3.left * Time.deltaTime * 8;
+            } else if (attackPause > 15)
+            {
+                blast2.transform.position = bomb2.transform.position;
+                bomb2.SetActive(false);
+            } else if (attackPause > 13.5)
+            {
+                bomb2.SetActive(true);
+                bomb2.transform.position = new Vector3(15, -4);
+                blast2.transform.position = new Vector3(15, -4);
+            } else if (attackPause > 12.5)
+            {
+                banjoSwipe.transform.position = new Vector3(0, 0);
+            } else if (attackPause > 11.5)
+            {
+                banjoSwipe.transform.position = new Vector3(0, 10);
+            } else if (attackPause > 10)
+            {
+                attackPause = 30;
             }
         }
         void CLAttacks()
@@ -116,12 +140,21 @@ public class AttackManager : MonoBehaviour
         {
 
         }
-
         if (attackPause > 0)
         {
-            attackPause -= Time.deltaTime;
+            attackPause -= Time.deltaTime * 2;
         }
     }
+
+    void OneRandom(float int1, float int2)
+    {
+        if (ifRand)
+        {
+            randInt = Random.Range(int1, int2);
+            ifRand = false;
+        }
+    }
+
     void InstantiateEverything()
     {
         bullet1 = Instantiate(CObullet, new Vector3(-2, 11), Quaternion.identity);
@@ -131,8 +164,9 @@ public class AttackManager : MonoBehaviour
         splitBullet2 = Instantiate(COsplitBullet2, new Vector3(0, 10), Quaternion.identity);
         swipe = Instantiate(COslash, new Vector3(0, 10), Quaternion.identity);
         bomb = Instantiate(CObomb, new Vector3(-15, -2), Quaternion.identity);
-        bomb2 = Instantiate(CObomb, new Vector3(15, -2), Quaternion.identity);
+        bomb2 = Instantiate(CObomb, new Vector3(15, -4), Quaternion.identity);
         blast = Instantiate(COboom, new Vector3(-15, -2), Quaternion.identity);
-        blast2 = Instantiate(COboom, new Vector3(15, -2), Quaternion.identity);
+        blast2 = Instantiate(COboom, new Vector3(15, -4), Quaternion.identity);
+        banjoSwipe = Instantiate(CObanjoswipe, new Vector3(0, 10), Quaternion.identity);
     }
 }
